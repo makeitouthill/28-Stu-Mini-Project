bf9383cb11fd7ba28e5f39a9cb8ceed1f7ba8bb1
Alex Horning <67214695+makeitouthill@users.noreply.github.com>
2023-03-02 14:20:22 -0800

    Added attribute to onclick and classname in Bucket.js

--- Develop/bucket-list/src/components/Bucket.js
+++ Develop/bucket-list/src/components/Bucket.js
@@ -13,7 +13,7 @@ function Bucket(props) {
   const submitUpdate = (value) => {
 
     // TODO: Write logic to update the `edit` value in state after a user updates an entry in the list
-
+    
     // TODO: Set the key:value pairs in the `edit` object back to empty strings
 
   };
@@ -23,21 +23,21 @@ function Bucket(props) {
     return <BucketForm edit={edit} onSubmit={submitUpdate} />;
   }
 
-  return props.bucket.map((item, index) => (
+  return props.bucket.map((item, index, condition) => (
     // TODO: Add a className of `bucket row complete ${item.eagerness}` for completed items, and `bucket-row ${item.eagerness}` for non-completed items
     // TODO: Add a key attribute set to the value of the index position
     // Hint: use a ternary operator
-    <div className={ } key={}>
+    <div className={condition ? `bucket row complete ${item.eagerness}`: `bucket-row ${item.eagerness}`} key={index}>
 
-      // TODO: Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument
-      <div key={} onClick={}>
+      {/* // TODO: Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument */}
+      <div key={item.id} onClick={`completeBucketItem(item.id)`}>
           {/* TODO: Add the item text here */}
       </div>
       <div className="icons">
-        // TODO: Add an onClick event update the `edit` object with the `id`, `value`, and `eagerness` properties
-        <p onClick={}> ✏️</p>
+        {/* // TODO: Add an onClick event update the `edit` object with the `id`, `value`, and `eagerness` properties */}
+        <p onClick={setEdit}> ✏️</p>
         {/* TODO: Add an onClick event that will invoke the removeBucketItem method passing in the `item.id` */}
-        <p onClick={}> 🗑️</p>
+        <p onClick={setEdit}> 🗑️</p>
       </div>
     </div>
   ));
